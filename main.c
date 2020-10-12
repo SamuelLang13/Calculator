5b374f164b9e8f063907f4e2e63e20b8a144f15c
SamuelLang <langsamuel10@gmail.com>
2020-10-12 19:49:15 +0200

    Switch OK

--- main.c
+++ main.c
@@ -3,7 +3,30 @@
 int main() {
     double num1;
     double num2;
-    double sum;
+    int sum;
     char operation;
+    printf("Zadejte vzorec:\n");
+    scanf(" %lf  %c  %lf =",&num1,&operation,&num2);
+    switch (operation) {
+        case '+':
+            sum=num1+num2;
+            printf("%f\n",sum);
+            break;
+        case '-':
+            sum=num1-num2;
+            printf("%f\n",sum);
+            break;
+        case '*':
+            sum=num1*num2;
+            printf("%f\n",sum);
+            break;
+        case '/':
+            sum=num1/num2;
+            printf("%f\n",sum);
+            break;
+        default:
+            printf("Nespravny vstup.");
+            break;
+    }
     return 0;
 }
