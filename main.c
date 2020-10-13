98f2f67a7090707e197544961b93ada90d7393bd
SamuelLang <langsamuel10@gmail.com>
2020-10-13 11:43:45 +0200

    Uprava switch, pridanie podmienok

--- main.c
+++ main.c
@@ -11,19 +11,84 @@ int main() {
     char operation;
 
     printf("Zadejte rovnici:\n");
-    if(scanf(" %lf %c %lf = %lf",&num1,&operation,&num2,&operation)!=4)
-    {
-        printf("Nespravny vstup.\n");
-    }
-    else
+    if(scanf("%lf %c %lf = %lf",&num1,&operation,&num2,&sum)==4)
     {
         switch (operation)
         {
             case '+':
+            {
                 pcsum=num1+num2;
+                if(fabs(pcsum-sum)<=1000*DBL_EPSILON)
+                {
+                    printf("Rovnice je spravne.\n");
+                }
+                else
+                {
+                    printf("%f != %f",pcsum,sum);
+                }
+                break;
+            }
+
+            case '-':
+            {
+                pcsum=num1-num2;
+                if(fabs(pcsum-sum)<=1000*DBL_EPSILON)
+                {
+                    printf("Rovnice je spravne.\n");
+                }
+                else
+                {
+                    printf("%f != %f",pcsum,sum);
+                }
+                break;
+            }
+
+            case '*':
+            {
+                pcsum=num1*num2;
+                if(fabs(pcsum-sum)<=1000*DBL_EPSILON)
+                {
+                    printf("Rovnice je spravne.\n");
+                }
+                else
+                {
+                    printf("%f != %f",pcsum,sum);
+                }
+                break;
+            }
+            case '/':
+            {
+                if(num2!=0)
+                {
+                    pcsum=num1/num2;
+                    if(fabs(pcsum-sum)<=1000*DBL_EPSILON)
+                    {
+                        printf("Rovnice je spravne.\n");
+                    }
+                    else
+                    {
+                        printf("%f != %f",pcsum,sum);
+                    }
+                }
+                else
+                {
+                    printf("Nespravny vstup.\n");
+                }
+
+                break;
+            }
+
+            default:
+            {
+                printf("Nespravny vstup.\n");
+            }
 
 
         }
+    }
+    else
+    {
+        printf("Nespravny vstup.\n");
     }
 
     return 0;
